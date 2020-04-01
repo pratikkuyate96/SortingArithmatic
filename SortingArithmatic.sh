9c6f8035e852370317ae90b3fc31a0f9960ca299
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-01 22:00:05 +0530

    ADDED: Addition substraction

--- SortingArithmatic.sh
+++ SortingArithmatic.sh
@@ -11,3 +11,7 @@ echo "Result 1 : "$resultExp1
 #Compute multiplication and addition
 resultExp2=$(( a * b + c ))
 echo "Result 2 : "$resultExp2
+
+#Compute addition and substraction
+resultExp3=$(( c + a / b ))
+echo "Result 3 : "$resultExp3
