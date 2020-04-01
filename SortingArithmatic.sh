fed53247ec5440a800d374ec95ae4253f604e787
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-01 20:16:43 +0530

    ADDED: Multiplication addition

--- SortingArithmatic.sh
+++ SortingArithmatic.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -x
 
+#Read value a, b, c
 read -p "Enter three values : " a b c
 echo "Three values are : " $a $b $c
 
+#Compute addtion and multiplication
 resultExp1=$(( a + b * c ))
-echo "Result : "$resultExp1
+echo "Result 1 : "$resultExp1
+
+#Compute multiplication and addition
+resultExp2=$(( a * b + c ))
+echo "Result 2 : "$resultExp2
