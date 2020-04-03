8d762f29ffdd54e5517029087e0540ecaf3e8cb0
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-03 18:13:21 +0530

    MODIFIED: Changed code for dictionary

--- SortingArithmatic.sh
+++ SortingArithmatic.sh
@@ -1,27 +1,27 @@
-#!/bin/bash -x
+#!/bin/bash
 
-#Read value a, b, c
-read -p "Enter three values : " a b c
-echo "Three values are : " $a $b $c
-
-#Compute addtion and multiplication
-resultExp1=`echo "scale=3; $a + $b * $c" | bc`
-
-#Compute multiplication and addition
-resultExp2=`echo "scale=3; $a * $b + $c" | bc`
-
-#Compute addition and substraction
-resultExp3=`echo "scale=3; $c + $a / $b" | bc`
-
-#Compute mod and addition
-resultExp4=`echo "scale=3; $a % $b + $c" | bc`
+#Compute the expressions
+function compute() {
+	resultExp1=`echo "scale=2; $(($a + $b * $c ))" | bc`
+	resultExp2=`echo "scale=2; $(($a * $b + $c ))" | bc`
+	resultExp3=`echo "scale=2; $(($c + $a / $b ))" | bc`
+	resultExp4=`echo "scale=2; $(($a % $b + $c ))" | bc`
+}
 
 #Store result in dictionary
-declare -A expResultDict
-expResultDict[exp1]=$resultExp1
-expResultDict[exp2]=$resultExp2
-expResultDict[exp3]=$resultExp3
-expResultDict[exp4]=$resultExp4
+function dictionary() {
+	expResultDict[exp1]=$resultExp1
+	expResultDict[exp2]=$resultExp2
+	expResultDict[exp3]=$resultExp3
+	expResultDict[exp4]=$resultExp4
+}
 
-echo "Keys : ${!expResultDict[@]}"
-echo "Value : ${expResultDict[@]}"
+function arithmaticComputation() {
+	compute
+	dictionary
+}
+
+#Read value a, b, c
+read -p "Enter three value : " a b c
+echo "Three values are : " $a $b $c
+arithmaticComputation
